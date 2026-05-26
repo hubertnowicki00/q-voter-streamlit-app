01b67fead75a658569eac618513471e48ff00b71
Hubert Nowicki <huber.nowicki@vp.pl>
2026-05-26 15:35:04 +0200

    Update wakeup.py

--- wakeup.py
+++ wakeup.py
@@ -3,7 +3,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,6 +15,7 @@ import traceback
 url = os.getenv("STREAMLIT_APP_URL", "https://q-voter-app-app-54xgh5gxxrr4pukoxwfmwe.streamlit.app/")
 
 chrome_options = Options()
+chrome_options.binary_location = "/usr/bin/chromium-browser"
 chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
@@ -23,7 +23,7 @@ chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--remote-debugging-port=922")
 try:
-    service = Service(ChromeDriverManager().install())
+    service = Service("/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.get(url)
     wait = WebDriverWait(driver, 15)
